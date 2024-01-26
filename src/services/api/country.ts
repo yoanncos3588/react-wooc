7131fcdac86a8af7091ba99d4567ba4302d1d3ad
YoannCos <yoanncos@gmail.com>
2024-01-26 15:27:11 +0100

    add customer to api

--- src/services/api/country.ts
+++ src/services/api/country.ts
@@ -1,5 +1,8 @@
 import { axiosInstanceWoo } from "./api";
 
+/**
+ * fetch list of countries from WC api
+ */
 const country = {
   get: async () => {
     return await axiosInstanceWoo.get("/data/countries");
