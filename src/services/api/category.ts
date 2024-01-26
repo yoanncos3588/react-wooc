8ffd3c72f9d1161a029842a46a20f1501f1715a6
YoannCos <yoanncos@gmail.com>
2024-01-26 15:07:56 +0100

    clean api category

--- src/services/api/category.ts
+++ src/services/api/category.ts
@@ -1,17 +1,13 @@
+import { UrlParams } from "../../types/apiParams";
 import { axiosInstanceWoo } from "./api";
 
 const category = {
   /**
-   * fetch 100 first categories (100 is the max limit with wc api). Check pagination if categories could be > 100
+   * Fetch products categories
+   * https://woocommerce.github.io/woocommerce-rest-api-docs/#product-categories
    */
-  getAll: async () => {
-    return await axiosInstanceWoo.get(`/products/categories?per_page=100`);
-  },
-  /**
-   * fetch categories by page
-   */
-  getByPage: async (page: string) => {
-    return await axiosInstanceWoo.get(`/products/categories?page=${page}`);
+  get: async (params?: UrlParams) => {
+    return await axiosInstanceWoo.get(`/products/categories`, { params });
   },
 };
 
