776e7900cc596fdc9944e0e2cdf36bbef53d5c51
YoannCos <yoanncos@gmail.com>
2024-02-09 17:55:49 +0100

    Add local token management and create axios interceptors file

---
+++ src/provider/authProvider.ts
@@ -0,0 +1,8 @@
+import { createContext, useState } from "react";
+
+// const AuthContext = createContext(null);
+
+// const AuthProvider = ({children}) => {
+//   const [token, setToken] = useState(localStorage.getItem('token'));
+
+// }
