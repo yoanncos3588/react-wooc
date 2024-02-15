901f3ecd0be2851150573bdd24a2a18251593f0b
YoannCos <yoanncos@gmail.com>
2024-02-15 14:53:32 +0100

    refactor filterproducts component

--- src/components/FilterProducts.tsx
+++ src/components/FilterProducts.tsx
@@ -1,55 +1,54 @@
 import { FormControl, FormControlLabel, Grid, InputLabel, MenuItem, Select, SelectChangeEvent, Switch } from "@mui/material";
 import { useSearchParams } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
+import { filtersProductsQueryKeys } from "../services/filters/products";
 
 const FilterProducts = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const orderKey = filtersProductsQueryKeys.order;
+  const orderByKey = filtersProductsQueryKeys.orderBy;
+  const onSaleKey = filtersProductsQueryKeys.onSale;
 
-  const defaultOrder = { label: "Plus récents", value: "order=date&orderby=asc", orderby: "asc", order: "date" };
+  const defaultOrder = { label: "Plus récents", value: `${orderKey}=date&${orderByKey}=asc`, orderby: "asc", order: "date" };
 
   const orderByValues = [
     defaultOrder,
-    { label: "Plus anciens ", value: "order=date&orderby=desc", orderby: "desc", order: "date" },
-    { label: "Nom (A-z)", value: "order=title&orderby=asc", orderby: "asc", order: "title" },
-    { label: "Nom (Z-a)", value: "order=title&orderby=desc", orderby: "desc", order: "title" },
-    { label: "Plus chers", value: "order=price&orderby=asc", orderby: "asc", order: "price" },
-    { label: "Moins chers", value: "order=price&orderby=desc", orderby: "desc", order: "price" },
+    { label: "Plus anciens ", value: `${orderKey}=date&${orderByKey}=desc`, orderby: "desc", order: "date" },
+    { label: "Nom (A-z)", value: `${orderKey}=title&${orderByKey}=asc`, orderby: "asc", order: "title" },
+    { label: "Nom (Z-a)", value: `${orderKey}=title&${orderByKey}=desc`, orderby: "desc", order: "title" },
+    { label: "Plus chers", value: `${orderKey}=price&${orderByKey}=asc`, orderby: "asc", order: "price" },
+    { label: "Moins chers", value: `${orderKey}=price&${orderByKey}=desc`, orderby: "desc", order: "price" },
   ];
 
-  const order = searchParams.get("order");
-  const orderBy = searchParams.get("orderby");
-  const onSale = searchParams.get("on_sale");
-  const orderByActivValue = orderByValues.find((item) => item.value === `order=${order}&orderby=${orderBy}`);
+  const orderBySelectValue = orderByValues.find(
+    (item) => item.value === `${orderKey}=${searchParams.get(orderKey)}&${orderByKey}=${searchParams.get(orderByKey)}`
+  );
 
   const handleChange = (e: SelectChangeEvent) => {
-    const selected = orderByValues.find((item) => e.target.value === `order=${item.order}&orderby=${item.orderby}`);
+    const selected = orderByValues.find((item) => e.target.value === `${orderKey}=${item.order}&${orderByKey}=${item.orderby}`);
 
-    setSearchParams((searchParams) => {
-      if (selected) {
-        if (selected.value === defaultOrder.value) {
-          searchParams.delete("orderby");
-          searchParams.delete("order");
-        } else {
-          searchParams.set("orderby", selected.orderby);
-          searchParams.set("order", selected.order);
+    setSearchParams(
+      (searchParams) => {
+        if (selected) {
+          if (selected.value === defaultOrder.value) {
+            searchParams.delete(orderByKey);
+            searchParams.delete(orderKey);
+          } else {
+            searchParams.set(orderByKey, selected.orderby);
+            searchParams.set(orderKey, selected.order);
+          }
         }
-      }
-      return searchParams;
-    });
+        return searchParams;
+      },
+      { preventScrollReset: true }
+    );
   };
 
   const handleChangeSale = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.checked) {
-      setSearchParams((searchParams) => {
-        searchParams.set("on_sale", "true");
-        return searchParams;
-      });
-    } else {
-      setSearchParams((searchParams) => {
-        searchParams.delete("on_sale");
-        return searchParams;
-      });
-    }
+    setSearchParams((searchParams) => {
+      e.target.checked ? searchParams.set(onSaleKey, "true") : searchParams.delete(onSaleKey);
+      return searchParams;
+    });
   };
 
   return (
@@ -61,7 +60,7 @@ const FilterProducts = () => {
             <Select
               labelId="products-orderby"
               id="products-orderby-select"
-              value={orderByActivValue ? orderByActivValue.value : defaultOrder.value}
+              value={orderBySelectValue ? orderBySelectValue.value : defaultOrder.value}
               label="Trier par"
               onChange={handleChange}
             >
@@ -74,7 +73,7 @@ const FilterProducts = () => {
           </FormControl>
         </Grid>
         <Grid item xs={12} md={4}>
-          <FormControlLabel control={<Switch color="warning" onChange={handleChangeSale} checked={!!onSale} />} label="Promo" />
+          <FormControlLabel control={<Switch color="warning" onChange={handleChangeSale} checked={!!searchParams.get(onSaleKey)} />} label="Promo" />
         </Grid>
       </Grid>
     </>
---
+++ src/services/filters/products.ts
@@ -0,0 +1,5 @@
+export const filtersProductsQueryKeys = {
+  order: "order",
+  orderBy: "orderby",
+  onSale: "on_sale",
+};
