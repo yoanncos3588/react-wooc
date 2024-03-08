12adf32e8d752938c34dea978b6dd54569719063
YoannCos <yoanncos@gmail.com>
2024-03-08 16:32:28 +0100

    clear forgotten code used for testing

--- src/components/CartButton.tsx
+++ src/components/CartButton.tsx
@@ -2,14 +2,10 @@ import { Badge, IconButton } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { useCart } from "../hooks/useCart";
-import { useEffect } from "react";
 
 function CartButton() {
   const { cart } = useCart();
 
-  useEffect(() => {
-    console.log("cart");
-  }, [cart]);
   return (
     <IconButton component={RouterLink} to="#" aria-label="mon panier" size="large" sx={{ px: 2 }} edge="end">
       <Badge badgeContent={cart.length} color="warning">
